4e200a2bac15ba889ed85aa8162f3419e5361b61
m4milaad <mb4milad.bhattt@gmail.com>
2025-01-29 14:08:00 +0530

    added quiz game

---
+++ Quiz Game/main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_object = Question(question=question["text"], answer=question["answer"])
+    question_bank.append(question_object)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
+
+if quiz.score == quiz.question_number:
+    print("You won")
---
+++ Quiz Game/quiz_brain.py
@@ -0,0 +1,23 @@
+class QuizBrain:
+    def __init__(self, bank):
+        self.question_number = 0
+        self.question_list = bank
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        user = input(f"Q{self.question_number + 1}. {self.question_list[self.question_number].question} (True/False)")
+        self.check_answer(user.lower(), self.question_list[self.question_number].answer.lower())
+        self.question_number += 1
+        print(f"Your score is {self.score}/{self.question_number}\n\n")
+
+
+    def check_answer(self, user, answer):
+        if user == answer:
+            self.score += 1
+            print("That's right")
+        else:
+            print("That's Wrong")
+        print(f"The correct answer was {answer.title()}")
