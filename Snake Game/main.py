e632739769a1f0f1ef5b82fa35e1cc4d54997c64
m4milaad <mb4milad.bhattt@gmail.com>
2025-02-07 23:21:00 +0530

    changes

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -9,34 +9,33 @@ snake = Snake()
 food = Food()
 screen.register_keys(snake)
 
+
+def action():
+    choice = screen.take_input()
+    if choice == "t":
+        snake.reset()
+        score.reset()
+    elif choice == "q":
+        score.reset()
+        screen.screen.bye()
+    else:
+        print("Invalid choice")
+        score.reset()
+        screen.screen.bye()
+
+
 game_on = True
 while game_on:
     screen.screen_update()
     snake.move()
     if snake.segments[0].distance(food) < 15:
-        # this loop detects collision with food and increments the scoreboard by 1
         food.refresh()
         score.add_score()
         snake.extend()
     if snake.out_of_boundaries():
-        # game_on = False
-        choice = screen.take_input()
-        if choice == "t":
-            snake.reset()
-            score.reset()
-        elif choice == "q":
-            game_on = False
-        else:
-            print("Invalid choice")
-            game_on = False
-        snake.reset()
-        score.reset()
-        # score.game_over()
+        action()
     for seg in snake.segments[1:]:
         if snake.segments[0].distance(seg) < 10:
-            # game_is_on = False
-            score.reset()
-            snake.reset()
-            # score.game_over()
+            action()
 
 screen.exit()
